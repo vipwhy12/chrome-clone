b4e68a928dde7a9d580f49daeb4d736f42d39d08
Yuna <85014628+vipwhy12@users.noreply.github.com>
2022-07-17 17:22:45 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -5,13 +5,13 @@ const API_KEY = "70d7fbefb4e6d883b48092346cf0173c";
 
 function onGeoOK(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
+    const log = position.coords.longitude;
     console.log("You Live in", lat, lng);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then((data) =>{
         city.innerText = data.name;
         weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-      });
+    });
     
 }
 
